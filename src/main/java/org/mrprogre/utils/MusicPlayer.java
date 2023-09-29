25148d714538fff48d6954ca725973170a1f09b0
Dmitry <rps_project@mail.ru>
2023-09-29 10:05:05 +0300

    + voice in baby mode

--- src/main/java/org/mrprogre/utils/MusicPlayer.java
+++ src/main/java/org/mrprogre/utils/MusicPlayer.java
@@ -4,17 +4,17 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 import javazoom.jl.player.advanced.PlaybackEvent;
 import javazoom.jl.player.advanced.PlaybackListener;
 
-import java.io.FileInputStream;
-import java.util.Objects;
+import java.io.InputStream;
 
 public class MusicPlayer {
     private int pausedOnFrame = 0;
 
     public void play(String path) {
         try {
-            FileInputStream fis = new FileInputStream(Objects.requireNonNull(MusicPlayer.class.getResource(path)).getPath());
-
+            InputStream fis = MusicPlayer.class.getResourceAsStream(path);
+            assert fis != null;
             AdvancedPlayer player = new AdvancedPlayer(fis);
+
             player.setPlayBackListener(new PlaybackListener() {
                 @Override
                 public void playbackFinished(PlaybackEvent event) {
